27874882fe66863cc716ca1fe4e5f2842849d7be
reinaldo <rjperalta07@soy.sena.edu.co>
2026-05-26 09:58:13 -0500

    se quito a anguie pq se veia mal xd

--- src/lib/data/team.ts
+++ src/lib/data/team.ts
@@ -7,10 +7,9 @@ export interface TeamMember {
 
 export const teamMembers: TeamMember[] = [
     { id: "1", name: "Carlos Mojica", role: "Programador", image: "/images/equipo/carlos_programador.webp" },
-    { id: "2", name: "Angie Lopez", role: "JovenPro Ferias", image: "/images/equipo/angie_ferias.webp" },
-    { id: "3", name: "Lucia López", role: "Coordinadora Inventarios JovenPro Local", image: "/images/equipo/coordinadora_lucia.webp" },
-    { id: "4", name: "César González", role: "Gerente General", image: "/images/equipo/cesar_gerente.webp" },
-    { id: "5", name: "Alejandra González", role: "Dirección Marketing & Redes", image: "/images/equipo/alejandra_marketing.webp" },
-    { id: "6", name: "Shabby Galindo", role: "Coordinadora Bogotá DC", image: "/images/equipo/shabby_coordinadora.webp" },
-    { id: "7", name: "Laura Pacheco", role: "Coordinadora JovenPro Local", image: "/images/equipo/laura_coordinadora.webp" },
+    { id: "2", name: "Lucia López", role: "Coordinadora Inventarios JovenPro Local", image: "/images/equipo/coordinadora_lucia.webp" },
+    { id: "3", name: "César González", role: "Gerente General", image: "/images/equipo/cesar_gerente.webp" },
+    { id: "4", name: "Alejandra González", role: "Dirección Marketing & Redes", image: "/images/equipo/alejandra_marketing.webp" },
+    { id: "5", name: "Shabby Galindo", role: "Coordinadora Bogotá DC", image: "/images/equipo/shabby_coordinadora.webp" },
+    { id: "6", name: "Laura Pacheco", role: "Coordinadora JovenPro Local", image: "/images/equipo/laura_coordinadora.webp" },
 ];
